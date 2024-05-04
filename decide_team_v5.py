a6d0e5a0c0257e3f2536fd270ae790aa19540c0e
LightsByDennis <108363591+LightsByDennis@users.noreply.github.com>
2024-05-04 20:12:44 +0200

    Update decide_team_v5.py

--- decide_team_v5.py
+++ decide_team_v5.py
@@ -1,65 +1,178 @@
-import multiprocessing as mp
-import cv2 as cv
+import cv2
+import time
 import numpy as np
+from multiprocessing import Process
+from multiprocessing import Queue
 from ultralytics import YOLO
 
-model = YOLO('./yolov8n_saved_model/yolov8n_full_integer_quant.tflite') #TFLOW model
+from PIL import Image
+import argparse
 
-# Define functions for each task
+from log import logger
 
-def capture_frames(queue):
-    # Your frame capture logic here
+# define the function that handles our processing thread
+def process_video(model_path:str,video_source,pwm_gpio:int,show:bool=True,enable_motor:bool=False):
+
+    global model
+    motor_pos = [0,45,90,135,180]
+    motor_index = 2
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    queuepulls = 0.0
+    detections = 0
+    fps = 0.0
+    qfps = 0.0
+    # init video
+    cap = cv2.VideoCapture(video_source)
+    
+
+    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    logger.info(f"[info] W, H, FPS\n{frameWidth}, {frameHeight}, {cap.get(cv2.CAP_PROP_FPS)}")
+
+    # initialize the input queue (frames), output queue (out),
+    # and the list of actual detections returned by the child process
+    inputQueue = Queue(maxsize=1)
+    outputQueue = Queue(maxsize=1)
+    img = None
+    out = None
+    model = YOLO('./yolov8n_saved_model/yolov8n_full_integer_quant.tflite', task="detect")
+
+    # construct a child process *indepedent* from our main process of
+    # execution
+    logger.info("Starting process...")
+    p = Process(target=classify_frame, args=(img, inputQueue, outputQueue,))
+    p.daemon = True
+    p.start()
+    time.sleep(10)
+    logger.info("Starting capture...")
+
+    # time the frame rate....
+    timer1 = time.time()
+    frames = 0
+    queuepulls = 0
+    timer2 = 0
+    t2secs = 0
+
+    while (cap.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        if ret == True:
+
+            if queuepulls == 1:
+                timer2 = time.time()
+            # Capture frame-by-frame
+            # frame = frame.array
+            img = Image.fromarray(frame)
+            # if the input queue *is* empty, give the current frame to
+            # classify
+            if inputQueue.empty():
+                inputQueue.put(frame)
+
+            # if the output queue *is not* empty, grab the detections
+            if not outputQueue.empty():
+                out = outputQueue.get()
+
+            if out is not None:
+                ball_xC = 0
+                ball_yC = 0
+                # loop over the detections
+                for box  in out:
+                    print(str(box[5]))
+                    xmin = int(box[0])
+                    ymin = int(box[1])
+                    xmax = int(box[2])
+                    ymax = int(box[3])
+                    objID = int(box[5])
+                    confidence = box[4]
+                    ball_xC += (xmin+xmax)/2
+                    ball_yC += (ymin+ymax)/2
+
+                    if confidence > confThreshold:
+                        # bounding box
+                        cv2.rectangle(frame, (xmin, ymin),
+                                    (xmax, ymax), color=(0, 0, 255))
+                        detections += 1  # positive detections
+                if len(out)>0:
+                    ball_xC /= len(out)
+                    ball_yC /= len(out)
+                cv2.circle(frame,(int(ball_xC),int(ball_yC)), 5, (0,0,255), -1)            
+                queuepulls += 1
+
+            if show:
+                # Display the resulting frame
+                cv2.rectangle(frame, (0, 0),
+                            (frameWidth, 20), (0, 0, 0), -1)
+
+                cv2.rectangle(frame, (0, frameHeight-20),
+                            (frameWidth, frameHeight), (0, 0, 0), -1)
+                cv2.putText(frame, 'Threshold: '+str(round(confThreshold, 1)), (10, 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 255), 1, cv2.LINE_AA)
+
+                cv2.putText(frame, 'VID FPS: '+str(fps), (frameWidth-80, 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 255), 1, cv2.LINE_AA)
+
+                cv2.putText(frame, 'TPU FPS: '+str(qfps), (frameWidth-80, 20),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 255), 1, cv2.LINE_AA)
+
+                cv2.putText(frame, 'Positive detections: '+str(detections), (10, frameHeight-10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 255), 1, cv2.LINE_AA)
+
+                cv2.putText(frame, 'Elapsed time: '+str(round(t2secs, 2)), (150, frameHeight-10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 255), 1, cv2.LINE_AA)
+
+                cv2.namedWindow('Coral', cv2.WINDOW_NORMAL)
+                cv2.resizeWindow('Coral', frameWidth, frameHeight)
+                cv2.imshow('Coral', frame)
+                cv2.waitKey(1)
+
+            
+            # FPS calculation
+            frames += 1
+            if frames >= 1:
+                end1 = time.time()
+                t1secs = end1-timer1
+                fps = round(frames/t1secs, 2)
+            if queuepulls > 1:
+                end2 = time.time()
+                t2secs = end2-timer2
+                qfps = round(queuepulls/t2secs, 2)
+
+        # Break the loop
+        else:
+            break
+
+    p.join()
+    # Everything done, release the vid
+    cap.release()
+
+    cv2.destroyAllWindows()
+
+def classify_frame(img, inputQueue, outputQueue):
+    global model
+    global confThreshold
     while True:
-        vcap = cv.VideoCapture(0)  # Example function to capture frames
-        frame = vcap.read()
-        queue.put(frame)
+        # check to see if there is a frame in our input queue
+        if not inputQueue.empty():
+            # grab the frame from the input queue
+            img = inputQueue.get()
+            objs = model.predict(img,conf=confThreshold,verbose=False)[0]
+            outputQueue.put(objs.cpu().numpy().boxes.data)
 
-def detect_objects(frame_queue, detection_queue):
-    # Your object detection logic here
-    while True:
-        frame = frame_queue.get()
-        detections = model(frame,stream=True) #detect objects in frame trough neural network  # Example function for object detection
-        detection_queue.put(detections)
+import configparser
 
-def control_turret(detection_queue):
-    # Your turret control logic here
-    while True:
-        detection = detection_queue.get()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', help='path to cfg file', default="config.cfg")
+    config = configparser.ConfigParser()
 
-        for detected in detection: 
-
-            boxes = detected.boxes
-
-            for box in boxes:
-
-                x1, y1, x2, y2 = box.xyxy[0]
-
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-
-                if int(box.cls[0]) == 0:
-
-                    person_arr = np.array([x1,y1,x2,y2,box.conf[0].cpu().numpy()]) #get data about detection into format required by tracker
-
-                    people = np.vstack((people,person_arr))
-
-        # Process detections and control turret accordingly
-
-if __name__ == '__main__':
-    # Create queues for inter-process communication
-    frame_queue = mp.Queue()
-    detection_queue = mp.Queue()
-
-    # Create processes for each task
-    frame_process = mp.Process(target=capture_frames, args=(frame_queue,))
-    detection_process = mp.Process(target=detect_objects, args=(frame_queue, detection_queue))
-    turret_process = mp.Process(target=control_turret, args=(detection_queue,))
-
-    # Start processes
-    frame_process.start()
-    detection_process.start()
-    turret_process.start()
-
-    # Join processes
-    frame_process.join()
-    detection_process.join()
-    turret_process.join()
+    # Load the configuration file
+    args = parser.parse_args()
+    config.read(args.cfg)
+    modelPath = config["detector"]["model_path"]
+    camera_idx = config["detector"]["camera_idx"]
+    confThreshold = config["detector"].getfloat("conf_threshold")
+    pwm_gpio = config["motor"].getint("gpio_pin")
+    show = config["general"].getboolean("show")
+    enable_motor = config["motor"].getboolean('enable')
+    process_video(model_path=modelPath,video_source=camera_idx,pwm_gpio=pwm_gpio,show=show,enable_motor=enable_motor)
